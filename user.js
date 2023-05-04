3890f8bca57c8b8ce8d336b6660bf4f16224bc00
emrnd223 <129907832+emrnd223@users.noreply.github.com>
2023-05-04 16:14:25 -0700

    Add files via upload

---
+++ user.js
@@ -0,0 +1,12 @@
+user_pref("apz.gtk.touchpad_pinch.enabled", false);
+user_pref("apz.touch_move_tolerance", "2.0");
+user_pref("apz.touch_start_tolerance", "1.0");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.tabs.allowTabDetach", false);
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.tabs.insertRelatedAfterCurrent", false);
+user_pref("browser.tabs.remote.autostart", false);
+user_pref("browser.tabs.remote.desktopbehavior", false);
+user_pref("browser.tabs.remote.warmup.enabled", false);
+user_pref("browser.tabs.remote.warmup.maxTabs", 0);
+user_pref("devtools.policy.disabled", true);
