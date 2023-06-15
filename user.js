ac8bfc125a80c74d12f2935cb90dfde1a3476964
emrnd223 <129907832+emrnd223@users.noreply.github.com>
2023-06-15 11:17:41 -0700

    Update user.js
    fix for tap hold selection

--- user.js
+++ user.js
@@ -18,3 +18,4 @@ user_pref("browser.tabs.remote.warmup.enabled", false);
 user_pref("browser.tabs.remote.warmup.maxTabs", 0);
 user_pref("devtools.policy.disabled", true);
 user_pref("extensions.pocket.enabled", false);
+user_pref("ui.click_hold_context_menus.delay", 500000);
