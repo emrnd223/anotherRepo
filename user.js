77dc2d362135821bc18d146befe69accf462eb65
emrnd223 <129907832+emrnd223@users.noreply.github.com>
2023-06-15 10:55:45 -0700

    Update user.js
    fix for the double tap text select functionality

--- user.js
+++ user.js
@@ -1,5 +1,11 @@
 user_pref("app.update.auto", false);
+user_pref("apz.allow_double_tap_zooming", false);
+user_pref("apz.drag.enabled", false);
+user_pref("apz.drag.initial.enabled", false);
+user_pref("apz.drag.touch.enabled", false);
 user_pref("apz.gtk.touchpad_pinch.enabled", false);
+user_pref("apz.max_tap_time", 5);
+user_pref("apz.popups.enabled", false);
 user_pref("apz.touch_move_tolerance", "2.0");
 user_pref("apz.touch_start_tolerance", "1.0");
 user_pref("browser.newtabpage.enabled", false);
